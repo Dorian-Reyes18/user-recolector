eec2bf6e4ccc45863604a2dd0ba729b3f94882e8
Dorian Reyes <doreyes@gbm.net>
2025-09-23 19:27:30 -0600

    Aplicando configuracion de seguridad con heltmet para protger el protocolo http

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import helmet from "helmet";
 import routes from "./routes/index.js";
 import authRoutes from "./routes/auth.js";
 import usuariosSystem from "./routes/usuarios_system.js";
@@ -9,6 +10,22 @@ import { verifyToken, isAdmin } from "./middleware/authMiddleware.js";
 dotenv.config();
 
 const app = express();
+
+// Seguridad con headers HTTP
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"], // solo permite recursos del mismo origen
+        scriptSrc: ["'self'"], // scripts solo desde el mismo dominio
+        objectSrc: ["'none'"], // bloquea objetos <object>, <embed>, <applet>
+        upgradeInsecureRequests: [], // fuerza HTTPS si es posible
+      },
+    },
+    crossOriginEmbedderPolicy: false, // evita errores con algunas librerías modernas
+  })
+);
+
 app.use(cors());
 app.use(express.json());
 
